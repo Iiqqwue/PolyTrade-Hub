4f2d0538317dded0d9a9bb72ef78fdd75b0ae2ab
Андрей это я <par270105@gmail.com>
2024-05-01 16:15:59 +0300

    Add file

--- PolyTradeHub/PolyTradeHub.py
+++
@@ -1,9 +0,0 @@
-class User:
-    def __init__(self, login, password, email, name):
-        self.login = login
-        self.password = password
-        self.email = email
-        self.name = name
-user1 = User("damirosina", 123123123, "asdas@mail.ru", "Hasbulla")
-user2 = User("Andromeda", 12312, "par234234@gmail.com", "Abdurozik")
-print(user2.name)
